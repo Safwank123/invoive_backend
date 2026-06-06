3766baf2f6a47ba919a9f5d22d27a5e7eef1c4b4
Safwank123 <safwanmuhammed546@gmail.com>
2026-06-07 00:18:43 +0530

    chore: remove vercel, render, and unnecessary files

--- build_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install Python dependencies (--break-system-packages needed for Vercel's managed env)
-pip install -r requirements.txt --break-system-packages
-
-# Collect static files for WhiteNoise to serve
-python manage.py collectstatic --noinput
